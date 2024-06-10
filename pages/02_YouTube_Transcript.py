a18ce14b933c8ff0af779ed6eacaf83744057bb3
Chinmay Chopade <cchopade@gmail.com>
2024-06-10 14:08:21 +0530

    replaced pytube with yt-dlp

--- pages/02_YouTube_Transcript.py
+++ pages/02_YouTube_Transcript.py
@@ -4,14 +4,26 @@ Created on Sat Jun  8 07:44:24 2024
 
 @author: Chinmay.Chopade
 """
-import os
-os.system('python -m pip3 install --upgrade pytube')
+
 
 from pytube import YouTube
 import streamlit as st
 import openai
-
 from dotenv import load_dotenv
+import pafy
+import os
+import yt_dlp
+
+def download_audio(url, output_dir="downloads"):
+    # Create a new yt-dlp object
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,  # only keep the audio
+        'audioformat': 'mp4',  # convert to mp3
+        'outtmpl': f'/audio.mp4',  # output template
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 
 try:
@@ -38,26 +50,28 @@ if generate_btn:
     else:
         with st.spinner("Generating transcript..."):
 
-            #try:
-                video = YouTube(video_url)
-                st.success("Video fetched")
-                duration = video.length
+            try:
+                
+                download_audio(video_url)
+                #video = YouTube(video_url)
+                #st.success("Video fetched")
+                #duration = video.length
                 # filtering the audio. File extension can be mp4/webm
-                audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
-                st.success("Audio stream filtered")
-                audio.download(filename="audio.mp4")
+                #audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
+                #st.success("Audio stream filtered")
+                #audio.download(filename="audio.mp4")
                 st.success('Download Completed!')
                          
-            #except:
-                #st.write("Connection Error")  # to handle exception
+            except:
+                st.write("Connection Error")  # to handle exception
                 
-                audio_file = open("audio.mp4", "rb")
-                transcription_cost = round(round(duration/60,2)*0.006,4)
-                st.write(f'Estimated trasncription cost = USD {transcription_cost}')
+            audio_file = open("audio.mp4", "rb")
+            #transcription_cost = round(round(duration/60,2)*0.006,4)
+            #st.write(f'Estimated trasncription cost = USD {transcription_cost}')
             
-                transcript = openai.audio.transcriptions.create(
+            transcript = openai.audio.transcriptions.create(
                                 model="whisper-1",
                                 file=audio_file
                                 )
-                st.write(transcript.text)
+            st.write(transcript.text)
             
